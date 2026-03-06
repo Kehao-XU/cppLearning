9085b92837505d19a7f0543e85d662a5f0de5645
KehaoXU <xukehao84@gmail.com>
2026-03-06 12:38:49 +0800

    Skeleton: hw1

---
+++ hw1/hw1_1.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <ios>
+#include <string>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+int main() 
+{ 
+	//TODO
+	return 0; 
+} 
