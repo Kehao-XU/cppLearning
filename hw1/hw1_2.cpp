314d3789435d46ec079f92a7ecb942c53b4123d0
JustinianXU <25303050130@m.fudan.edu.cn>
2026-03-07 09:32:40 +0800

    NewItem: hw1_2 complete

--- hw1/hw1_2.cpp
+++ hw1/hw1_2.cpp
@@ -6,6 +6,47 @@ using namespace std;
 int main() 
 { 
 	//TODO
+	vector<string> names;
+	string name;
+	while (cin >> name) {
+		names.push_back(name);
+	}
+	auto size = names.size();
+	if (size == 0) {
+		cout << "Please input names" << endl;
+		return 1;
+	}
 
+	vector<int> longest, shortest;
+	longest.push_back(0);
+	shortest.push_back(0);
+	for (int i = 1; i < size; i++) {
+		if (names[i].size() > names[longest[0]].size()) {
+			longest.clear();
+			longest.push_back(i);
+		}
+		else if (names[i].size() == names[longest[0]].size()) {
+			longest.push_back(i);
+		}
+
+		else if (names[i].size() < names[shortest[0]].size()) {
+			shortest.clear();
+			shortest.push_back(i);
+		}
+		else if (names[i].size() == names[shortest[0]].size()) {
+			shortest.push_back(i);
+		}
+	}
+
+	cout << "longest: ";
+	for (int i : longest) {
+		cout << names[i];
+	}
+	cout << endl;
+	cout << "shortest: ";
+	for (int i : shortest) {
+		cout << names[i] << " ";
+	}
+	cout << endl;
 	return 0; 
 }
